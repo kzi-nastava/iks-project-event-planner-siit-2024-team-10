c1c105df8cff8136054a424faeeef31a6494306b
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-14 16:39:39 +0100

    Create login component and add it to routing

--- EventPlanner/src/app/app-routing.module.ts
+++ EventPlanner/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WinesComponent } from './wine/wines/wines.component';
-import { AddWineComponent } from './wine/add-wine/add-wine.component';
 import { HomeComponent } from './layout/home/home.component';
+import {LoginComponent} from './layout/login/login.component';
 
 const routes: Routes = [
-  {path: 'wine', component: WinesComponent},
-  {path: 'add', component: AddWineComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path:'login',component: LoginComponent},
 ];
 
 @NgModule({
