74f29fbaed1918b3e0ce02eda213f5c4b8f3d682
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-14 18:58:58 +0100

    Add manage offerings to routing and navbar

--- EventPlanner/src/app/app-routing.module.ts
+++ EventPlanner/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './layout/home/home.component';
 import {LoginComponent} from './layout/login/login.component';
+import {ManageOfferingsComponent} from './offering/manage-offerings/manage-offerings.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path:'login',component: LoginComponent},
+  {path:'manage-offerings',component:ManageOfferingsComponent},
 ];
 
 @NgModule({
