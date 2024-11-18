83351cc53d4a6cb58580c677826bfa41ffe40c86
milicaradicc <milica.t.radic@gmail.com>
2024-11-18 17:17:57 +0100

    Add create category dialog component

---
+++ EventPlanner/src/app/offering/create-category-dialog/create-category-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-category-dialog',
+  templateUrl: './create-category-dialog.component.html',
+  styleUrl: './create-category-dialog.component.css'
+})
+export class CreateCategoryDialogComponent {
+
+}
