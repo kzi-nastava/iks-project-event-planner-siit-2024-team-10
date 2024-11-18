223ce69c0c104e62a48a323096557b6017e2f914
milicaradicc <milica.t.radic@gmail.com>
2024-11-18 21:54:04 +0100

    Resolve conflict

--- EventPlanner/src/app/offering/offering.module.ts
+++ EventPlanner/src/app/offering/offering.module.ts
@@ -13,12 +13,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatButtonModule } from '@angular/material/button';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon'; 
-import { ManageOfferingsComponent } from './manage-offerings/manage-offerings.component';
 import { CreateOfferingsComponent } from './create-offerings/create-offerings.component';
 @NgModule({
   declarations: [
