36bae576d6fd70cc09d7c41fb0739e60bfbb252f
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-03 22:01:16 +0100

    Implement event details binding

--- EventPlanner/src/app/event/event-details/event-details.component.ts
+++ EventPlanner/src/app/event/event-details/event-details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {EventService} from '../event.service';
+import {Event} from '../model/event.model'
 
 @Component({
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
   styleUrl: './event-details.component.css'
 })
-export class EventDetailsComponent {
+export class EventDetailsComponent implements OnInit {
+  event: Event;
 
+  constructor(private route: ActivatedRoute, private eventService:EventService) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const id = +params['id'];
+      this.event=this.eventService.getEvent(id);
+    })
+  }
 }
