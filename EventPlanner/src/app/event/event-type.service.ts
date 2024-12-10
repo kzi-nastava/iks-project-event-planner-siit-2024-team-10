273edc0f6e4c13fd741a0006fe4962e828f4d4a2
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-10 18:31:49 +0100

    Implement add event type service method

--- EventPlanner/src/app/event/event-type.service.ts
+++ EventPlanner/src/app/event/event-type.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {EventType} from './model/event-type.model';
 import {Observable} from 'rxjs';
 import {environment} from '../../env/environment';
+import {CreateEventTypeDTO} from './model/create-event-type-dto.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,9 @@ export class EventTypeService {
 
   getAll() : Observable<EventType[]> {
     return this.httpClient.get<EventType[]>(environment.apiHost + `/event-types`);
+  }
+
+  add(eventType:CreateEventTypeDTO) : Observable<EventType> {
+    return this.httpClient.post<EventType>(environment.apiHost + "/event-types", eventType);
   }
 }
