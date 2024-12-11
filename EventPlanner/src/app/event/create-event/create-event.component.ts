0d67bec24567738eab6dd695a603221d17b6b96f
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-11 23:24:57 +0100

    Implement fetching event types

--- EventPlanner/src/app/event/create-event/create-event.component.ts
+++ EventPlanner/src/app/event/create-event/create-event.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Category} from '../../offering/model/category.model';
 import { EventType } from '../model/event-type.model';
+import {EventTypeService} from '../event-type.service';
 
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
   styleUrl: './create-event.component.css'
 })
-export class CreateEventComponent {
+export class CreateEventComponent implements OnInit{
   createForm: FormGroup = new FormGroup({
     eventType: new FormControl('', [Validators.required]),
     name: new FormControl('', [Validators.required]),
     description: new FormControl('', [Validators.required]),
     maxParticipants: new FormControl('', [Validators.required]),
-    isOpen: new FormControl(true, [Validators.required]),
+    eventPublicity: new FormControl(true, [Validators.required]),
     country: new FormControl('', [Validators.required]),
     city: new FormControl('', [Validators.required]),
     street: new FormControl('', [Validators.required]),
@@ -22,6 +23,22 @@ export class CreateEventComponent {
     date: new FormControl('', [Validators.required]),
   });
   allEventTypes:EventType[];
+
+  constructor(private eventTypeService: EventTypeService,) {}
+
+  ngOnInit(): void {
+    this.createForm.patchValue({eventPublicity:"open"})
+    this.eventTypeService.getAll().subscribe({
+      next: (eventTypes:EventType[]) => {
+        console.log(eventTypes[0].active);
+        this.allEventTypes = eventTypes.filter((x) => x.active);
+        console.log(this.allEventTypes);
+      },
+      error: (_) => {
+        console.log("Error loading categories")
+      }
+    });
+  }
 
   save():void{}
 }
