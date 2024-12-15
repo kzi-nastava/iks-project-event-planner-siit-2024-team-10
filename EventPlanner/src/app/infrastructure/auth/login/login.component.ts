31f2c4b1f52c5d239b1343858c8e89a7e84285db
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-15 16:09:46 +0100

    Implement login logic

--- EventPlanner/src/app/infrastructure/auth/login/login.component.ts
+++ EventPlanner/src/app/infrastructure/auth/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../../../user/user.service';
+import {LoginRequestDto} from '../model/login-request-dto.model';
+import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
+import {LoginResponseDTO} from '../model/login-response-dto.model';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +15,27 @@ export class LoginComponent {
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)])});
+  invalidCredentials=false;
 
-  constructor(private userService: UserService) {}
+  constructor(private authService:AuthService, private router:Router) {}
 
   login(){
-    if(!this.loginForm.valid)
-      return;
-    this.userService.login(this.loginForm.value.email, this.loginForm.value.password);
+    if(this.loginForm.valid){
+      const loginRequest: LoginRequestDto = {
+        email: this.loginForm.value.email || "",
+        password: this.loginForm.value.password || ""
+      }
+      this.authService.login(loginRequest).subscribe({
+        next: (response: LoginResponseDTO) => {
+          localStorage.setItem('user', response.accessToken);
+          this.authService.setUser()
+          this.router.navigate(['home'])
+        },
+        error:(err)=>{
+          this.invalidCredentials = true;
+        }
+      })
+    }
+
   }
 }
