b3c7ed7b307970738077ac0e03ee569f72e0d683
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-23 17:35:04 +0100

    Implement toggle favourite logic

--- EventPlanner/src/app/event/event-details/event-details.component.ts
+++ EventPlanner/src/app/event/event-details/event-details.component.ts
@@ -4,6 +4,7 @@ import {EventService} from '../event.service';
 import {Event} from '../model/event.model'
 import {AgendaItem} from '../model/agenda-item.model';
 import {Observable} from 'rxjs';
+import {AccountService} from '../../account/account.service';
 
 @Component({
   selector: 'app-event-details',
@@ -14,8 +15,12 @@ export class EventDetailsComponent implements OnInit {
   event: Event;
   agenda:AgendaItem[];
   userRating:number;
+  isFavourite:boolean=false;
 
-  constructor(private route: ActivatedRoute, private eventService:EventService) {
+  constructor(
+    private route: ActivatedRoute,
+    private eventService:EventService,
+    private accountService: AccountService) {
   }
 
   getStarArray(rating: number): number[] {
@@ -24,6 +29,29 @@ export class EventDetailsComponent implements OnInit {
 
   setRating(rating: number): void {
     this.userRating = rating;
+  }
+
+  toggleFavourite(): void {
+    if(this.isFavourite){
+      this.accountService.removeEventFromFavourites(this.event.id).subscribe({
+        next: () => {
+          this.isFavourite = !this.isFavourite;
+        },
+        error: (err) => {
+          console.error('Error adding event to favourites:', err);
+        }
+      });
+    }
+    else {
+      this.accountService.addEventToFavourites(this.event.id).subscribe({
+        next: () => {
+          this.isFavourite = !this.isFavourite;
+        },
+        error: (err) => {
+          console.error('Error removing event from favourites:', err);
+        }
+      });
+    }
   }
 
   ngOnInit(): void {
@@ -44,6 +72,14 @@ export class EventDetailsComponent implements OnInit {
         },
         error: (err) => {
           console.error('Error fetching event agenda:', err);
+        }
+      });
+      this.accountService.isInFavouriteEvents(id).subscribe({
+        next: (isFavourite: boolean) => {
+          this.isFavourite = isFavourite;
+        },
+        error: (err) => {
+          console.error('Error fetching favourite event:', err);
         }
       });
     })
