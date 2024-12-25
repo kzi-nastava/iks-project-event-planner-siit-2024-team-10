0917c4f90f003bdcba9756ebdf0bfeb134bb3d57
milicaradicc <milica.t.radic@gmail.com>
2024-12-25 15:46:16 +0100

    Implement edit

--- EventPlanner/src/app/offering/edit-service/edit-service.component.ts
+++ EventPlanner/src/app/offering/edit-service/edit-service.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
-import { CreateCategoryDialogComponent } from '../create-category-dialog/create-category-dialog.component';
 import { OfferingService } from '../offering-service/offering.service';
+import { ServiceService } from '../service-service/service.service';
+import { EditServiceDTO } from '../model/edit-service-dto.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-edit-service',
@@ -12,21 +14,11 @@ import { OfferingService } from '../offering-service/offering.service';
 })
 export class EditServiceComponent implements OnInit {
   offeringForm: FormGroup;
-  // ovo ce se drugacije ucitavati kasnije, trenutno je ovako jer eventovi nisu kreirani
-  eventTypes = [
-    'Wedding',
-    'Birthday',
-    'Corporate',
-    'Anniversary'
-  ];
-  selectedEventTypes: Set<string> = new Set();
   timeOptions = [1, 2, 3, 4, 5];
-
+  snackBar: MatSnackBar = inject(MatSnackBar);
   constructor(
     private fb: FormBuilder,
-    private dialog: MatDialog,
-    private route: ActivatedRoute,
-    private offeringService: OfferingService
+    private serviceService: ServiceService
   ) {}
 
   ngOnInit(): void {
@@ -39,14 +31,14 @@ export class EditServiceComponent implements OnInit {
 
   initForm(): void {
     this.offeringForm = this.fb.group({
-      serviceCategory: ['', Validators.required],
+      id: [null],
       name: ['', Validators.required],
       description: ['', Validators.required],
       specification: [''],
       price: ['', [Validators.required, Validators.min(0)]],
       discount: ['', [Validators.min(0), Validators.max(100)]],
       photos: [[]],
-      timeType: ['fixed'], // Dodato polje za tip vremena
+      timeType: ['fixed'], 
       fixedTime: [''],
       minTime: [''],
       maxTime: [''],
@@ -55,12 +47,13 @@ export class EditServiceComponent implements OnInit {
       isAvailable: [true],
       isVisible: [true]
     });
+    console.log(this.offeringForm.valid);
   }
 
   prefillForm(data: any): void {
     const timeType = data.fixedTime ? 'fixed' : 'flexible';
     this.offeringForm.patchValue({
-      serviceCategory: data.serviceCategory || '',
+      id: data.id || null,
       name: data.name || '',
       description: data.description || '',
       specification: data.specification || '',
@@ -75,19 +68,7 @@ export class EditServiceComponent implements OnInit {
       isAvailable: data.isAvailable !== undefined ? data.isAvailable : true,
       isVisible: data.isVisible !== undefined ? data.isVisible : true,
     });
-  
-    // labels for events
-    if (data.eventTypes) {
-      data.eventTypes.forEach((type: string) => this.selectedEventTypes.add(type));
-    }
   }  
-  
-
-  openDialog(): void {
-    this.dialog.open(CreateCategoryDialogComponent, {
-      width: '350px'
-    });
-  }
 
   onPhotoUpload(event: any): void {
     const files = event.target.files;
@@ -95,34 +76,30 @@ export class EditServiceComponent implements OnInit {
     if (photosControl) {
       photosControl.setValue([...files]);
     }
-  }
-
-  toggleSelection(type: string): void {
-    if (this.selectedEventTypes.has(type)) {
-      this.selectedEventTypes.delete(type);
-    } else {
-      this.selectedEventTypes.add(type);
-    }
-  }
-
-  isSelected(type: string): boolean {
-    return this.selectedEventTypes.has(type);
+    console.log(this.offeringForm.valid);
   }
 
   onSubmit(): void {
     if (this.offeringForm.valid) {
-      const formData = {
+      const formData : EditServiceDTO = {
         ...this.offeringForm.value,
-        eventTypes: Array.from(this.selectedEventTypes), 
       };
+
+      console.log(formData)
+      this.serviceService.edit(formData).subscribe({
+        next: (response) => {
+          this.snackBar.open('Service edited successfully', 'OK', { 
+            duration: 3000 
+          });
+          },
+        error: (error) => {
+          console.error('Error creating service:', error);
   
-      this.offeringService.editService(formData);
-  
-      this.offeringForm.reset();
-      this.selectedEventTypes.clear();
-      alert('Form data edited successfully!');
-    } else {
-      alert('Please fill in all required fields correctly.');
+          this.snackBar.open('Failed to create service. Please try again.', 'Dismiss', {
+            duration: 3000
+          });
+        }
+      });
     }
   }
 }
