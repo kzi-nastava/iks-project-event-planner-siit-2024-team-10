304cbbac957b50960f67416b3cd91b08bf02e9f1
Mijat Krivokapić <mijat2004@gmail.com>
2025-01-17 17:12:10 +0100

    Implement update user service method

--- EventPlanner/src/app/user/user.service.ts
+++ EventPlanner/src/app/user/user.service.ts
@@ -4,6 +4,8 @@ import {Event} from '../event/model/event.model';
 import {environment} from '../../env/environment';
 import {GetUserDTO} from './model/get-user-dto.model';
 import {HttpClient} from '@angular/common/http';
+import {UpdateUserDTO} from './model/update-user-dto.model';
+import {UpdatedUSerDTO} from './model/updated-user-dto.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,9 @@ export class UserService {
 
   getUser(accountId:number): Observable<GetUserDTO> {
     return this.httpClient.get<GetUserDTO>(environment.apiHost+'/users/'+accountId);
+  }
+
+  updateUser(accountId:number, updateUserDTO:UpdateUserDTO):Observable<UpdatedUSerDTO> {
+    return this.httpClient.put<UpdatedUSerDTO>(environment.apiHost+'/users/'+accountId, updateUserDTO);
   }
 }
