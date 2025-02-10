d22896c5cc09a39c2ca61b52f98fdb5b6d00e419
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2025-02-10 11:51:04 +0100

    Add notification pagination

--- EventPlanner/src/app/notification/notifications-page/notifications-page.component.ts
+++ EventPlanner/src/app/notification/notifications-page/notifications-page.component.ts
@@ -11,10 +11,10 @@ import { AuthService } from '../../infrastructure/auth/auth.service';
 export class NotificationsPageComponent implements OnInit {
   notifications: AppNotification[] = [];
   accountId: number;
-  noNotificationsMessage :boolean;
+  noNotificationsMessage :string;
   notificationPageProperties = {
     page: 0,
-    pageSize: 10,
+    pageSize: 5,
     totalPages: 0,
     totalElements: 0
   };
@@ -29,20 +29,29 @@ export class NotificationsPageComponent implements OnInit {
 
   fetchNotifications(): void {
     const { page, pageSize } = this.notificationPageProperties;
+    console.log(page, pageSize, this.notificationPageProperties.totalPages);
     this.notificationService.getAll(page, pageSize, this.accountId).subscribe({
       next: (response) => {
         this.notifications = response.content;
         this.notificationPageProperties.totalPages = response.totalPages;
         this.notificationPageProperties.totalElements = response.totalElements;
-        this.noNotificationsMessage = this.notifications.length === 0;
+        this.noNotificationsMessage = this.notifications.length ? '' : 'No notifications to show.';
       },
       error:() =>{
-
+        this.noNotificationsMessage = 'An error occurred while fetching events.';
       }
     });
   }
   nextPage(): void {
+    if (this.notificationPageProperties.page < this.notificationPageProperties.totalPages - 1) {
+      this.notificationPageProperties.page++;
+      this.fetchNotifications();
+    }
   }
   previousPage(): void {
+    if (this.notificationPageProperties.page > 0) {
+      this.notificationPageProperties.page--;
+      this.fetchNotifications();
+    }
   }
 }
