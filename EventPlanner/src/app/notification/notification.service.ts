8c5e15486e0d6d11951c97a2a3cb5c05450c0942
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2025-02-10 11:19:52 +0100

    Update not found page

--- EventPlanner/src/app/notification/notification.service.ts
+++ EventPlanner/src/app/notification/notification.service.ts
@@ -1,69 +1,30 @@
 import { Injectable } from '@angular/core';
 import { AppNotification } from './model/notification.model'
 import { Observable, of } from 'rxjs';
-
-const NOTIFICATIONS: AppNotification[] = [
-  {
-    id: 1,
-    title: 'New Rating',
-    content: 'User1 has rated your event "Concert at the Park" 4 stars.',
-    date: new Date('2024-11-01T12:00:00'),
-    isRead: false,
-  },
-  {
-    id: 2,
-    title: 'New Comment',
-    content: 'User2 has commented on your event "Charity Run". "Looking forward to it!"',
-    date: new Date('2024-11-02T14:00:00'),
-    isRead: false,
-  },
-  {
-    id: 3,
-    title: 'New Rating',
-    content: 'User3 has rated your event "Music Festival" 5 stars.',
-    date: new Date('2024-11-03T16:00:00'),
-    isRead: true,
-  },
-  {
-    id: 4,
-    title: 'Service Reminder',
-    content: 'Reminder: The service "Live Band" is due 1 hour.',
-    date: new Date('2024-11-04T10:00:00'),
-    isRead: true,
-  },
-  {
-    id: 5,
-    title: 'New Comment',
-    content: 'User4 has commented on your event "Outdoor Movie Night". "Can’t wait for the movie to start!"',
-    date: new Date('2024-11-05T18:30:00'),
-    isRead: true,
-  },
-  {
-    id: 6,
-    title: 'New Rating',
-    content: 'User5 has rated your event "Food Festival" 3 stars.',
-    date: new Date('2024-11-06T09:00:00'),
-    isRead: true,
-  },
-  {
-    id: 7,
-    title: 'New Rating',
-    content: 'User6 has rated your event "Holiday Party" 2 stars.',
-    date: new Date('2024-11-07T11:30:00'),
-    isRead: true,
-  },
-];
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../../env/environment';
+import { PagedResponse } from '../event/model/paged-response.model';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
-  private notificationList: AppNotification[];
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() {this.notificationList = [...NOTIFICATIONS];}
-
-  getAll(): Observable<AppNotification[]> {
-    return of(this.notificationList);
+  getAll(page: number, pageSize: number, accountId: number): Observable<PagedResponse<AppNotification>> {
+    let params = new HttpParams()
+          .set('page', page.toString())
+          .set('size', pageSize.toString());
+    return this.httpClient.get<PagedResponse<AppNotification>>(environment.apiHost+'/notifications/'+accountId, {params: params});
+  }
+  readNotification(notificationId: number): Observable<AppNotification> {
+    return this.httpClient.post<AppNotification>('/notifications/'+notificationId+'/read',null);
+  }
+  readAll(accountId: number): Observable<AppNotification[]> {
+    return this.httpClient.post<AppNotification[]>('/notifications/'+accountId+'/readAll',null);
+  }
+  toggleNotification(accountId: number): Observable<AppNotification> {
+    return this.httpClient.post<AppNotification>('/notifications/'+accountId+'/toggle',null);
   }
 }
--- EventPlanner/src/app/notification/notifications-page/notifications-page.component.ts
+++ EventPlanner/src/app/notification/notifications-page/notifications-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppNotification } from '../model/notification.model';
 import { NotificationService } from '../notification.service';
+import { AuthService } from '../../infrastructure/auth/auth.service';
 
 @Component({
   selector: 'app-notifications-page',
@@ -9,16 +10,39 @@ import { NotificationService } from '../notification.service';
 })
 export class NotificationsPageComponent implements OnInit {
   notifications: AppNotification[] = [];
+  accountId: number;
+  noNotificationsMessage :boolean;
+  notificationPageProperties = {
+    page: 0,
+    pageSize: 10,
+    totalPages: 0,
+    totalElements: 0
+  };
 
-  constructor(private notificationService: NotificationService) {}
+  constructor(private notificationService: NotificationService,
+              private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.accountId = this.authService.getAccountId();
     this.fetchNotifications();
   }
 
   fetchNotifications(): void {
-    this.notificationService.getAll().subscribe((data) => {
-      this.notifications = data;
+    const { page, pageSize } = this.notificationPageProperties;
+    this.notificationService.getAll(page, pageSize, this.accountId).subscribe({
+      next: (response) => {
+        this.notifications = response.content;
+        this.notificationPageProperties.totalPages = response.totalPages;
+        this.notificationPageProperties.totalElements = response.totalElements;
+        this.noNotificationsMessage = this.notifications.length === 0;
+      },
+      error:() =>{
+
+      }
     });
+  }
+  nextPage(): void {
+  }
+  previousPage(): void {
   }
 }
