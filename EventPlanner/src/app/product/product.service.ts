b3b0281259b5c78516174ccca6a2657edfa4b688
Mijat Krivokapić <mijat2004@gmail.com>
2025-07-20 22:05:10 +0200

    Add edit product service method

--- EventPlanner/src/app/product/product.service.ts
+++ EventPlanner/src/app/product/product.service.ts
@@ -6,6 +6,8 @@ import {Event} from '../event/model/event.model';
 import {environment} from '../../env/environment';
 import {CreateProductDTO} from './model/create-product-dto.model';
 import {Product} from '../offering/model/product.model';
+import {UpdateProductDTO} from './model/update-product-dto.model';
+import {UpdatedProductDTO} from './model/updated-product-dto.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +18,13 @@ export class ProductService {
 
   create(product:CreateProductDTO) : Observable<Product> {
     return this.httpClient.post<Product>(environment.apiHost + "/products", product);
+  }
+
+  update(id:number, product:UpdateProductDTO) : Observable<UpdatedProductDTO> {
+    return this.httpClient.put<UpdatedProductDTO>(environment.apiHost + "/products/"+id, product);
+  }
+
+  get(id:number) : Observable<Product> {
+    return this.httpClient.get<Product>(environment.apiHost + `/products/` + id);
   }
 }
